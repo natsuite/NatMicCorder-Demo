9edaa4525bd6f6d9d03c80386bf88e4359c926fd
Yusuf Olokoba <olokobayusuf@gmail.com>
2019-11-14 17:45:55 -0500

    Minor compiler fix

--- Assets/Scripts/NatMicCorder.cs
+++ Assets/Scripts/NatMicCorder.cs
@@ -11,87 +11,87 @@ using NatShareU;
 
 public class NatMicCorder : MonoBehaviour, IAudioProcessor {
 
-	#region --Op vars--
+    #region --Op vars--
 
-	[Header("Recording")]
-	public int videoWidth = 1280;
+    [Header("Recording")]
+    public int videoWidth = 1280;
     public int videoHeight = 720;
 
-	[Header("Sharing")]
-	public bool shareRecordings;
+    [Header("Sharing")]
+    public bool shareRecordings;
 
-	[Header("UI")]
-	public RawImage rawImage;
-	public AspectRatioFitter aspectFitter;
+    [Header("UI")]
+    public RawImage rawImage;
+    public AspectRatioFitter aspectFitter;
 
     private MP4Recorder videoRecorder;
     private RealtimeClock recordingClock;
-	private CameraInput cameraInput;
+    private CameraInput cameraInput;
 
-	private IAudioDevice audioDevice;
-	#endregion
+    private IAudioDevice audioDevice;
+    #endregion
 
 
-	#region --Recording--
+    #region --Recording--
 
-	public void StartRecording () {
-		var sampleRate = 44100;
-		var channelCount = 1;
+    public void StartRecording () {
+        var sampleRate = 44100;
+        var channelCount = 1;
         // Start recording from the main camera
-		recordingClock = new RealtimeClock();
+        recordingClock = new RealtimeClock();
         videoRecorder = new MP4Recorder(videoWidth, videoHeight, 30, sampleRate, channelCount, OnRecording);
-		cameraInput = new CameraInput(videoRecorder, recordingClock, Camera.main);
+        cameraInput = new CameraInput(videoRecorder, recordingClock, Camera.main);
         // Start the microphone
-		audioDevice = AudioDevice.GetDevices()[0];
-		audioDevice.StartRecording(sampleRate, channelCount, this);
-	}
+        audioDevice = AudioDevice.GetDevices()[0];
+        audioDevice.StartRecording(sampleRate, channelCount, this);
+    }
 
-	public void StopRecording () {
-		// Stop the microphone
-		audioDevice.StopRecording();
-		// Stop recording
-		cameraInput.Dispose();
-		videoRecorder.Dispose();
+    public void StopRecording () {
+        // Stop the microphone
+        audioDevice.StopRecording();
+        // Stop recording
+        cameraInput.Dispose();
+        videoRecorder.Dispose();
         cameraInput = null;
         videoRecorder = null;
-	}
-	#endregion
+    }
+    #endregion
 
 
-	#region --Callbacks--
+    #region --Callbacks--
 
-	// Invoked by Unity when the scene opens
-	private void Start () {
-		// Start the camera preview with NatCam
+    // Invoked by Unity when the scene opens
+    private void Start () {
+        // Start the camera preview with NatCam
         var cameraDevice = CameraDevice.GetDevices()[0];
         cameraDevice.StartPreview(previewTexture => {
             rawImage.texture = previewTexture;
-            aspectFitter.aspectRatio = preview.width / (float)preview.height;
+            aspectFitter.aspectRatio = previewTexture.width / (float)previewTexture.height;
         });
-	}
+    }
 
-	// Invoked by NatMic audio device with new audio sample buffer
-	public void OnSampleBuffer (float[] sampleBuffer, int sampleRate, int channelCount, long timestamp) {
-		// Send sample buffers directly to the video recorder for recording
-		videoRecorder.CommitSamples(sampleBuffer, recordingClock.Timestamp);
-	}
+    // Invoked by NatMic audio device with new audio sample buffer
+    public void OnSampleBuffer (float[] sampleBuffer, int sampleRate, int channelCount, long timestamp) {
+        // Send sample buffers directly to the video recorder for recording
+        videoRecorder.CommitSamples(sampleBuffer, recordingClock.Timestamp);
+    }
 
-	// Invoked by NatCorder video recorder once video recording is complete
-	private void OnRecording (string path) {
-		Debug.Log("Recording saved to path: "+path);
-		// Share
-		if (shareRecordings)
-			NatShare.Share(path);
-		// Playback the recording
-		else {
-			#if UNITY_EDITOR
-			UnityEditor.EditorUtility.OpenWithDefaultApp(path);
-			#elif UNITY_IOS
-			Handheld.PlayFullScreenMovie("file://" + path);
-			#elif UNITY_ANDROID
-			Handheld.PlayFullScreenMovie(path);
-			#endif
-		}
-	}
-	#endregion
+    // Invoked by NatCorder video recorder once video recording is complete
+    private void OnRecording (string path) {
+        Debug.Log("Recording saved to path: "+path);
+        // Share
+        if (shareRecordings)
+            NatShare.Share(path);
+        // Playback the recording
+        else {
+            #if UNITY_EDITOR
+            UnityEditor.EditorUtility.OpenWithDefaultApp(path);
+            #elif UNITY_IOS
+            Handheld.PlayFullScreenMovie("file://" + path);
+            #elif UNITY_ANDROID
+            Handheld.PlayFullScreenMovie(path);
+            #endif
+        }
+    }
+    #endregion
 }
